b57b5dae84e908c4797753f41f75f2e281e4dc80
Marco Pizzocaro <m.pizzocaro@inrim.it>
2024-09-17 18:53:58 +0200

    Add fitting functions

--- src/large_lattice_model/__init__.py
+++ src/large_lattice_model/__init__.py
@@ -2,6 +2,7 @@
 A python package to model the motional spectra of atoms trapped in a one-dimensional optical lattice.
 """
 
+from .fit import *
 from .latticemodel import *
 from .sidebands import *
 from .xyz import *
